0bfe684e3b3cf242026636fd6aa924396c428baa
KTachibanaM <whj19931115@gmail.com>
2022-11-10 17:53:56 -0800

    small fix for app global state

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:pill_city_flutter/src/api/app_global_state.dart';
 import 'package:pill_city_flutter/src/app.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AppGlobalState(),
+      child: const App(),
+    ),
+  );
 }
