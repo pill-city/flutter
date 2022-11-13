8992c0bfa0ec59d92933c0410defad15dc255787
KTachibanaM <whj19931115@gmail.com>
2022-11-13 14:22:02 -0800

    link preview style changes

--- lib/src/widgets/link_preview_widget.dart
+++ lib/src/widgets/link_preview_widget.dart
@@ -12,38 +12,38 @@ class LinkPreviewWidget extends StatelessWidget {
     if (linkPreview.state != LinkPreviewStateEnum.fetched) {
       return const SizedBox.shrink();
     }
+    Uri? parsedUrl;
+    try {
+      parsedUrl = Uri.parse(linkPreview.url);
+    } on FormatException {}
+
     return InkWell(
         onTap: () {
-          try {
-            launchUrl(
-              Uri.parse(linkPreview.url),
-              mode: LaunchMode.externalApplication,
-            );
-          } on FormatException {
+          if (parsedUrl == null) {
             return;
           }
+          launchUrl(
+            Uri.parse(linkPreview.url),
+            mode: LaunchMode.externalApplication,
+          );
         },
         child: Card(
           child: Padding(
             padding: const EdgeInsets.all(8),
             child: Row(
               children: [
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 48,
-                      child: linkPreview.imageUrls != null &&
-                              linkPreview.imageUrls!.isNotEmpty
-                          ? Image.network(
-                              linkPreview.imageUrls![0],
-                              errorBuilder: (context, error, stackTrace) =>
-                                  const Icon(Icons.broken_image),
-                            )
-                          : const Icon(Icons.link),
-                    ),
-                    const SizedBox(width: 8)
-                  ],
+                SizedBox(
+                  width: 48,
+                  child: linkPreview.imageUrls != null &&
+                          linkPreview.imageUrls!.isNotEmpty
+                      ? Image.network(
+                          linkPreview.imageUrls![0],
+                          errorBuilder: (context, error, stackTrace) =>
+                              const Icon(Icons.broken_image),
+                        )
+                      : const Icon(Icons.link),
                 ),
+                const SizedBox(width: 16),
                 Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -56,13 +56,27 @@ class LinkPreviewWidget extends StatelessWidget {
                           textScaleFactor: 0.9,
                         ),
                       if (linkPreview.subtitle != null)
-                        Text(
-                          linkPreview.subtitle!,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          textScaleFactor: 0.8,
-                          style: const TextStyle(color: Colors.grey),
-                        ),
+                        Column(children: [
+                          const SizedBox(height: 4),
+                          Text(
+                            linkPreview.subtitle!,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            textScaleFactor: 0.8,
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                        ]),
+                      if (parsedUrl != null)
+                        Column(children: [
+                          const SizedBox(height: 4),
+                          Text(
+                            parsedUrl.host,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            textScaleFactor: 0.7,
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                        ])
                     ],
                   ),
                 )
