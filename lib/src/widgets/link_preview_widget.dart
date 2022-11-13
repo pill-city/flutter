87ba2292cb63066b097bb86293d54ba5e863d5ce
KTachibanaM <whj19931115@gmail.com>
2022-11-13 02:00:04 -0800

    launch link in link preview

--- lib/src/widgets/link_preview_widget.dart
+++ lib/src/widgets/link_preview_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pill_city/pill_city.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LinkPreviewWidget extends StatelessWidget {
   const LinkPreviewWidget({super.key, required this.linkPreview});
@@ -11,51 +12,62 @@ class LinkPreviewWidget extends StatelessWidget {
     if (linkPreview.state != LinkPreviewStateEnum.fetched) {
       return const SizedBox.shrink();
     }
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Row(
-          children: [
-            if (linkPreview.imageUrls != null &&
-                linkPreview.imageUrls!.isNotEmpty)
-              Row(
-                children: [
-                  SizedBox(
-                    width: 48,
-                    child: Image.network(
-                      linkPreview.imageUrls![0],
-                      errorBuilder: (context, error, stackTrace) =>
-                          const Icon(Icons.broken_image),
-                    ),
+    return InkWell(
+        onTap: () {
+          try {
+            launchUrl(
+              Uri.parse(linkPreview.url),
+              mode: LaunchMode.externalApplication,
+            );
+          } on FormatException {
+            return;
+          }
+        },
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              children: [
+                if (linkPreview.imageUrls != null &&
+                    linkPreview.imageUrls!.isNotEmpty)
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 48,
+                        child: Image.network(
+                          linkPreview.imageUrls![0],
+                          errorBuilder: (context, error, stackTrace) =>
+                              const Icon(Icons.broken_image),
+                        ),
+                      ),
+                      const SizedBox(width: 8)
+                    ],
                   ),
-                  const SizedBox(width: 8)
-                ],
-              ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (linkPreview.title != null)
-                    Text(
-                      linkPreview.title!,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      textScaleFactor: 0.9,
-                    ),
-                  if (linkPreview.subtitle != null)
-                    Text(
-                      linkPreview.subtitle!,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      textScaleFactor: 0.8,
-                      style: const TextStyle(color: Colors.grey),
-                    ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (linkPreview.title != null)
+                        Text(
+                          linkPreview.title!,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          textScaleFactor: 0.9,
+                        ),
+                      if (linkPreview.subtitle != null)
+                        Text(
+                          linkPreview.subtitle!,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textScaleFactor: 0.8,
+                          style: const TextStyle(color: Colors.grey),
+                        ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
