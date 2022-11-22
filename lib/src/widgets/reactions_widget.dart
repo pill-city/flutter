d739bbb18c831ea6903eb8c0e0b58822bb23070a
KTachibanaM <whj19931115@gmail.com>
2022-11-21 16:33:02 -0800

    long press to show full reactions details

--- lib/src/widgets/reactions_widget.dart
+++ lib/src/widgets/reactions_widget.dart
@@ -1,10 +1,12 @@
 import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:pill_city/pill_city.dart';
 import 'package:pill_city_flutter/src/utils/get_user_names.dart';
 import 'package:pill_city_flutter/src/utils/rendered_reaction.dart';
 import 'package:pill_city_flutter/src/widgets/reaction_count_widget.dart';
 import 'package:pill_city_flutter/src/widgets/reaction_full_widget.dart';
+import 'package:twemoji/twemoji.dart';
 
 class ReactionsWidget extends StatelessWidget {
   const ReactionsWidget({Key? key, required this.reactions}) : super(key: key);
@@ -37,18 +39,84 @@ class ReactionsWidget extends StatelessWidget {
             .reduce((a, b) => a + b) <
         reactionFullDisplayNameTotalLengthThreshold;
 
-    return Row(
-      children: [
-        for (final reaction in renderedReactions)
-          Row(
-            children: [
-              useFull
-                  ? ReactionFullWidget(reaction: reaction)
-                  : ReactionCountWidget(reaction: reaction),
-              const SizedBox(width: 8),
-            ],
-          )
-      ],
+    return GestureDetector(
+      onLongPress: () {
+        showModalBottomSheet(
+          context: context,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(8),
+            ),
+          ),
+          builder: (BuildContext context) {
+            final List<Widget> children = [];
+            for (var i = 0; i < renderedReactions.length; i++) {
+              final renderedReaction = renderedReactions[i];
+              children.add(
+                Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Twemoji(
+                          emoji: renderedReaction.emoji,
+                          width: 16,
+                          height: 16,
+                        ),
+                        const SizedBox(width: 8),
+                        Text(renderedReaction.users.length.toString()),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      children: [
+                        Text(
+                          renderedReaction.users
+                              .map((u) => getUserPrimaryName(u))
+                              .join(", "),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+              if (i != renderedReactions.length - 1) {
+                children.add(const SizedBox(height: 16));
+              }
+            }
+
+            return Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Text(AppLocalizations.of(context)!.reactions),
+                  const SizedBox(height: 16),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: children,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+      child: Row(
+        children: [
+          for (final reaction in renderedReactions)
+            Row(
+              children: [
+                useFull
+                    ? ReactionFullWidget(reaction: reaction)
+                    : ReactionCountWidget(reaction: reaction),
+                const SizedBox(width: 8),
+              ],
+            )
+        ],
+      ),
     );
   }
 }
