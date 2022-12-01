749334a66cb8f13e0c5c25b7de56ab75392d4ebe
KTachibanaM <whj19931115@gmail.com>
2022-11-30 21:27:42 -0800

    redirect to web app for unimplemented features

--- lib/src/pages/scopes.dart
+++ lib/src/pages/scopes.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ScopesPage extends StatelessWidget {
   const ScopesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("scopes");
+    return GestureDetector(
+      onTap: () {
+        launchUrl(
+          Uri.parse("https://pill.city/circles"),
+          mode: LaunchMode.externalApplication,
+        );
+      },
+      child: Center(
+        child: Text(
+          AppLocalizations.of(context)!.use_this_feature_on_webapp,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            decoration: TextDecoration.underline,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/src/pages/users.dart
+++ lib/src/pages/users.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class UsersPage extends StatelessWidget {
   const UsersPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("users");
+    return GestureDetector(
+      onTap: () {
+        launchUrl(
+          Uri.parse("https://pill.city/users"),
+          mode: LaunchMode.externalApplication,
+        );
+      },
+      child: Center(
+        child: Text(
+          AppLocalizations.of(context)!.use_this_feature_on_webapp,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            decoration: TextDecoration.underline,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+    );
   }
 }
