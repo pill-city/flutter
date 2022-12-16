edb807f030c56dcbee0bf708b1dd776b7c03a575
KTachibanaM <whj19931115@gmail.com>
2022-12-16 05:38:06 -0800

    remove home state for now

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pill_city_flutter/src/app.dart';
 import 'package:pill_city_flutter/src/state/app_global_state.dart';
-import 'package:pill_city_flutter/src/state/home_state.dart';
 import 'package:pill_city_flutter/src/state/me_state.dart';
 import 'package:provider/provider.dart';
 
@@ -10,7 +9,6 @@ void main() {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => AppGlobalState()),
-        ChangeNotifierProvider(create: (context) => HomeState()),
         ChangeNotifierProvider(create: (context) => MeState()),
       ],
       child: App(),
--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,8 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:pill_city/pill_city.dart';
 import 'package:pill_city_flutter/src/pages/create_post.dart';
-import 'package:pill_city_flutter/src/state/home_state.dart';
+import 'package:pill_city_flutter/src/state/app_global_state.dart';
 import 'package:pill_city_flutter/src/utils/get_error_message.dart';
 import 'package:pill_city_flutter/src/widgets/loading_and_retry_widget.dart';
 import 'package:pill_city_flutter/src/widgets/post_widget.dart';
@@ -25,6 +26,7 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  List<Post> _posts = [];
   bool _loadingInitialPosts = true;
   DioError? _loadingInitialPostsError;
   bool _loadingMorePosts = false;
@@ -38,10 +40,29 @@ class HomePageState extends State<HomePage> {
     });
   }
 
+  bool _shouldShowPost(Post post) {
+    return !(post.deleted != null && post.deleted!) ||
+        !(post.blocked != null && post.blocked!) ||
+        !(post.reactions != null && post.reactions!.isNotEmpty) ||
+        !(post.comments != null && post.comments!.isNotEmpty);
+  }
+
   _loadInitialPosts() async {
+    if (_posts.isNotEmpty) {
+      return;
+    }
+
     try {
-      await Provider.of<HomeState>(context, listen: false)
-          .loadInitialPosts(context);
+      final appGlobalState =
+          Provider.of<AppGlobalState>(context, listen: false);
+      final api = await appGlobalState.getAuthenticatedApi();
+      final response = await api.getCoreApi().getHome();
+      if (response.data == null) {
+        return Future.error("Failed to load initial posts");
+      }
+      setState(() {
+        _posts = response.data!.where(_shouldShowPost).toList();
+      });
     } on DioError catch (errorCaught) {
       setState(() {
         _loadingInitialPostsError = errorCaught;
@@ -54,9 +75,21 @@ class HomePageState extends State<HomePage> {
   }
 
   _loadMorePosts() async {
+    if (_posts.isEmpty) {
+      return;
+    }
+
     try {
-      await Provider.of<HomeState>(context, listen: false)
-          .loadMorePosts(context);
+      final appGlobalState =
+          Provider.of<AppGlobalState>(context, listen: false);
+      final api = await appGlobalState.getAuthenticatedApi();
+      final response = await api.getCoreApi().getHome(fromId: _posts.last.id);
+      if (response.data == null) {
+        return Future.error("Failed to load more posts");
+      }
+      setState(() {
+        _posts.addAll(response.data!.where(_shouldShowPost));
+      });
     } on DioError catch (errorCaught) {
       setState(() {
         _loadingMorePostsError = errorCaught;
@@ -69,9 +102,23 @@ class HomePageState extends State<HomePage> {
   }
 
   Future<void> _loadLatestPosts() async {
+    if (_posts.isEmpty) {
+      return;
+    }
+
     try {
-      int newPosts = await Provider.of<HomeState>(context, listen: false)
-          .loadLatestPosts(context);
+      final appGlobalState =
+          Provider.of<AppGlobalState>(context, listen: false);
+      final api = await appGlobalState.getAuthenticatedApi();
+      final response = await api.getCoreApi().getHome(toId: _posts.first.id);
+      if (response.data == null) {
+        return Future.error("Failed to load more posts");
+      }
+      var data = response.data!.where(_shouldShowPost);
+      setState(() {
+        _posts = [...data, ..._posts];
+      });
+      int newPosts = data.length;
       if (!mounted) return;
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
@@ -110,66 +157,61 @@ class HomePageState extends State<HomePage> {
           children: [
             RefreshIndicator(
               onRefresh: _loadLatestPosts,
-              child: Consumer<HomeState>(
-                builder: (context, home, child) {
-                  return ListView.builder(
-                    controller: widget.scrollController,
-                    itemCount: home.posts.length + 1,
-                    itemBuilder: (context, index) {
-                      if (index < home.posts.length) {
-                        return Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8, bottom: 8),
-                              child: PostWidget(
-                                post: home.posts[index],
-                                contentMaxLines: homeContentMaxLines,
-                                maxLinkPreviews: homeMaxLinkPreviews,
-                                fullReactionMaxUsers: homeFullReactionMaxUsers,
-                                commentMaxLines: homeCommentMaxLines,
-                                maxComments: homeMaxComments,
-                                maxNestedComments: homeMaxNestedComments,
-                                showMedia: false,
-                              ),
-                            ),
-                            const Divider(
-                              thickness: 1,
-                            )
-                          ],
-                        );
-                      } else {
-                        if (_loadingMorePosts) {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        } else if (_loadingMorePostsError != null) {
-                          return Column(
-                            children: [
-                              Text(
-                                getErrorMessage(_loadingMorePostsError!),
-                              ),
-                              ElevatedButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _loadingMorePosts = true;
-                                    _loadingMorePostsError = null;
-                                  });
-                                  _loadMorePosts();
-                                },
-                                child:
-                                    Text(AppLocalizations.of(context)!.retry),
-                              ),
-                            ],
-                          );
-                        } else {
-                          _loadMorePosts();
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-                      }
-                    },
-                  );
+              child: ListView.builder(
+                controller: widget.scrollController,
+                itemCount: _posts.length + 1,
+                itemBuilder: (context, index) {
+                  if (index < _posts.length) {
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8, bottom: 8),
+                          child: PostWidget(
+                            post: _posts[index],
+                            contentMaxLines: homeContentMaxLines,
+                            maxLinkPreviews: homeMaxLinkPreviews,
+                            fullReactionMaxUsers: homeFullReactionMaxUsers,
+                            commentMaxLines: homeCommentMaxLines,
+                            maxComments: homeMaxComments,
+                            maxNestedComments: homeMaxNestedComments,
+                            showMedia: false,
+                          ),
+                        ),
+                        const Divider(
+                          thickness: 1,
+                        )
+                      ],
+                    );
+                  } else {
+                    if (_loadingMorePosts) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (_loadingMorePostsError != null) {
+                      return Column(
+                        children: [
+                          Text(
+                            getErrorMessage(_loadingMorePostsError!),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _loadingMorePosts = true;
+                                _loadingMorePostsError = null;
+                              });
+                              _loadMorePosts();
+                            },
+                            child: Text(AppLocalizations.of(context)!.retry),
+                          ),
+                        ],
+                      );
+                    } else {
+                      _loadMorePosts();
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  }
                 },
               ),
             ),
